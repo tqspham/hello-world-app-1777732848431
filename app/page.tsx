fb7636998553aa2d6e7c69fef86814b2e582544d
tqspham <tqs.pham@gmail.com>
2026-05-02 16:41:09 +0200

    Create app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="w-full px-4 text-center">
+      <h1 className="text-4xl font-normal text-black md:text-5xl lg:text-6xl">
+        Hello, World!
+      </h1>
+    </main>
+  );
+}
